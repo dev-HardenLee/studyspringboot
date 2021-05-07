0f8c725e440adad48c95bbf088d74a3d3bec66bf
dev-HardenLee <Harden.lee@companywe.co.kr>
2021-05-07 20:11:15 +0900

    API 테스트 코드 등록

---
+++ src/main/java/com/studyspringboot/admin/service/posts/PostsService.java
@@ -0,0 +1,20 @@
+package com.studyspringboot.admin.service.posts;
+
+import com.studyspringboot.admin.domain.posts.PostsRepository;
+import com.studyspringboot.admin.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto){
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+}
