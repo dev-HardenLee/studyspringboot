49ccd0447d298cbb451b9e9adf33b15d666a543d
dev-HardenLee <Harden.lee@companywe.co.kr>
2021-05-08 22:44:57 +0900

    게시판 CRUD API 완성

--- src/main/resources/static/js/app/index.js
+++ src/main/resources/static/js/app/index.js
@@ -3,6 +3,14 @@ const main = {
       $("#btn-save").on("click", () => {
           main.save();
       });
+
+      $("#btn-update").on("click", () => {
+          main.update();
+      });
+
+      $("#btn-delete").on("click", () => {
+          main.delete();
+      });
   },
   save : () => {
       const data = {
@@ -19,6 +27,42 @@ const main = {
          data:JSON.stringify(data)
       }).done(() => {
           alert("글이 등록되었습니다.");
+          window.location.href = "/";
+      }).fail(error => {
+          alert(JSON.stringify(error));
+      })
+  },
+  update : () => {
+      const data = {
+          title : $("#title").val(),
+          content : $("#content").val()
+      };
+
+      const id = $("#id").val();
+
+      $.ajax({
+          type:"put",
+          url:"/api/v1/posts/" + id,
+          dataType:"json",
+          contentType:"application/json;charset-utf-8",
+          data:JSON.stringify(data)
+      }).done(() => {
+          alert("글이 수정되었습니다.");
+          window.location.href = "/";
+      }).fail(error => {
+          alert(JSON.stringify(error));
+      })
+  },
+  delete : () => {
+      const id = $("#id").val();
+
+      $.ajax({
+          type:"delete",
+          url:"/api/v1/posts/" + id,
+          dataType:"json",
+          contentType:"application/json;charset-utf-8"
+      }).done(() => {
+          alert("글이 삭제되었습니다.");
           window.location.href = "/";
       }).fail(error => {
           alert(JSON.stringify(error));
